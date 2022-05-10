4897aceb3251ce5a6600e1bed4fba60fed69eb10
Konstantin Antonov <antonovk89@gmail.com>
2022-05-10 21:54:56 +0300

    tasks 2.2 done

--- Navigation/Navigation/Navigation/Profile/ProfileViewController.swift
+++ Navigation/Navigation/Navigation/Profile/ProfileViewController.swift
@@ -11,14 +11,35 @@ class ProfileViewController: UIViewController {
     
     let headerView = ProfileHeaderView()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .lightGray
-        view.addSubview(headerView)
-        headerView.backgroundColor = .lightGray
+    private func customHeaderView() {
+        headerView.translatesAutoresizingMaskIntoConstraints = false
     }
     
-    override func viewWillLayoutSubviews() {
-        headerView.frame = view.safeAreaLayoutGuide.layoutFrame
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(monkeyJobButton)
+        view.backgroundColor = .lightGray
+        view.addSubview(headerView)
+        customHeaderView()
+        setMonkeyJobButton()
+        headerView.backgroundColor = .lightGray
+        headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
+        headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
+        headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+        headerView.heightAnchor.constraint(equalToConstant: 220).isActive = true
+        
     }
+    private func setMonkeyJobButton() {
+        monkeyJobButton.translatesAutoresizingMaskIntoConstraints = false
+        monkeyJobButton.setTitle("Бесполезная кнопка", for: .normal)
+        monkeyJobButton.backgroundColor = .blue
+        monkeyJobButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
+        monkeyJobButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
+        monkeyJobButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0).isActive = true
+    }
+    
+    private lazy var monkeyJobButton : UIButton = {
+        let monkeyJobButton = UIButton()
+        return monkeyJobButton
+    }()
 }
